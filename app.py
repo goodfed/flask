0f6a9824a70df5102aeab2af6d91edd9a42e9486
goodfed <gorozhanko@gmail.com>
2020-10-07 21:57:14 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, abort
+
+import data
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_index():
+    output = render_template('index.html',
+                             title=data.title,
+                             subtitle=data.subtitle,
+                             description=data.description,
+                             departures=data.departures,
+                             tours=data.tours)
+    return output
+
+@app.route("/departures/<departure>/")
+def render_departure(departure):
+    if departure not in data.departures:
+        abort(404)
+
+    output = render_template('departure.html', departure=departure, departures=data.departures, tours=data.tours)
+    return output
+
+@app.route("/tours/<int:id>/")
+def render_tour(id):
+    if id not in data.tours:
+        abort(404)
+
+    output = render_template('tour.html', tour=data.tours[id], departures=data.departures)
+    return output
+
+
+if __name__ == '__main__':
+    app.run()
